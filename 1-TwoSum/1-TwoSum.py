6f9302d204391ad837b34912aa2b530c5dc02ac2
sindhiya75 <sindhiya1326@gmail.com>
2026-07-14 14:16:05 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,8 @@
+# Last updated: 14/07/2026, 14:16:06
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return i,j
+        
